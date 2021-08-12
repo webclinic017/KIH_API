094c1703e2e741c7a2ed7ebbbaa8506364dad553
Kavindu Athaudha <kavindu.athaudha@gmail.com>
2021-08-12 21:16:01 +1200

    Fixed some obvious bugs in global_common

--- global_common/__init__.py
+++ global_common/__init__.py
@@ -1,13 +1,11 @@
-import datetime
 import enum
-import json
+import os
+import subprocess
 import threading
 from decimal import Decimal
 from typing import Any, Dict, Type, Optional, Callable, List
 
-import database
-import pytz
-from ibkr.common import threads
+import logger
 
 
 def convert_string_to_dict(string: str) -> Dict[Any, Any]:
@@ -32,13 +30,14 @@ def get_enum_from_value(value: Any, enum: Type[enum.Enum]) -> Optional[Any]:
     return None
 
 
-def get_formatted_string_from_decimal(number: Decimal, decimal_places: int = 2) -> Decimal:
+def get_formatted_string_from_decimal(number: Decimal, decimal_places: int = 2) -> str:
     return f'{number.quantize(Decimal(10) ** -decimal_places):,}'
 
 
 def run_as_separate_thread(target: Callable, arguments: tuple = ()) -> None:
     return_function: threading.Thread = threading.Thread(target=target, args=arguments)
     return_function.start()
+
 
 def run_command(command_list: List[str]) -> List[str]:
     output_list: List[str] = []
@@ -47,9 +46,10 @@ def run_command(command_list: List[str]) -> List[str]:
         output_list.extend(subprocess.run(command.split(" "), stdout=subprocess.PIPE).stdout.decode("utf-8").split("\n"))
 
     for output in output_list:
-        logger.info(output_list)
+        logger.info(output)
 
     return output_list
+
 
 def get_running_processes(command: str) -> List[Dict[str, str]]:
     results_list: List[str] = run_command([f"sudo pgrep -a {command}"])
@@ -57,12 +57,13 @@ def get_running_processes(command: str) -> List[Dict[str, str]]:
     processes_list: List[Dict[str, str]] = []
     for result in results_list:
         if command in result:
-            processes_list.append({"PID": result.split[0], "Command": result.replace(result.split(" ")[0], "")})
+            processes_list.append({"PID": result.split("")[0], "Command": result.replace(result.split(" ")[0], "")})
 
     return processes_list
 
+
 def kill_process(keyword: str, command: str) -> bool:
-    processes_list: List[Dict[str, str]] = get_running_processes()
+    processes_list: List[Dict[str, str]] = get_running_processes(command)
     is_process_found_and_killed = False
 
     for process in processes_list:
@@ -72,7 +73,8 @@ def kill_process(keyword: str, command: str) -> bool:
 
     return is_process_found_and_killed
 
+
 def update_code_base(project_directory: str) -> None:
-    os.chdir(working_directory)
-    global_common.run_command(["git reset --hard origin/main"])
-    global_common.run_command(["git pull origin main"])
+    os.chdir(project_directory)
+    run_command(["git reset --hard origin/main"])
+    run_command(["git pull origin main"])
